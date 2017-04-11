340b163e47465391c1b5bc246c439668158981be
Tomasz <tom.slusarczyk@gmail.com>
2017-04-11 23:17:52 +0200

    dodałem klase ktora bedzie ustawiac wszystko do wyswietlania wykresu, łącznie z trzymaniem w niej ustawień

---
+++ KalkulatorPrzekroju/MainPlotView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace KalkulatorPrzekroju
+{
+    class MainPlotView
+    {
+        public OxyColor lineColor1 { get; set; }
+        public OxyColor lineColor2 { get; set; }
+        public OxyColor pointsColor { get; set; }
+        public double lineThickness1 { get; set; }
+        public double lineThickness2 { get; set; }
+        public double pointSize { get; set; }
+
+        public MainPlotView()
+        {
+
+        }
+    }
+}
