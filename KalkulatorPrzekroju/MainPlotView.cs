691d0ed977e34a532e06b79f5e850747ac134b9b
Tomasz <tom.slusarczyk@gmail.com>
2017-04-13 21:31:11 +0200

    funkcje generujace modele cd

--- KalkulatorPrzekroju/MainPlotView.cs
+++ KalkulatorPrzekroju/MainPlotView.cs
@@ -16,10 +16,52 @@ namespace KalkulatorPrzekroju
         public double lineThickness1 { get; set; }
         public double lineThickness2 { get; set; }
         public double pointSize { get; set; }
+        public double[][] dataPoints { private get; set; }
+
+        public PlotModel wykres { get; private set; }
 
         public MainPlotView()
         {
+            wykres = new PlotModel();
+        }
 
+        public void AddLineSerie(double[][] dataPoints, string name)
+        {
+            LineSeries punkty = new LineSeries();
+            punkty.Title = name;
+
+            for (int i = 0; i < dataPoints.Length; i++)
+            {
+                punkty.Points.Add(new DataPoint(dataPoints[i][1], dataPoints[i][0]));
+            }
+
+            wykres.Series.Add(punkty);
+        }
+
+        public void AddPointSerie(double[][] dataPoints, string name)
+        {
+            ScatterSeries punkty = new ScatterSeries();
+            punkty.Title = name;
+
+            for (int i = 0; i < dataPoints.Length; i++)
+            {
+                punkty.Points.Add(new ScatterPoint(dataPoints[i][1], dataPoints[i][0]));
+            }
+
+            wykres.Series.Add(punkty);
+        }
+
+        public void RemoveSerie(string name)
+        {
+            Series seriaToDelete = null;
+            foreach (Series seria in wykres.Series)
+            {
+                if (seria.Title == name)
+                {
+                    seriaToDelete = seria;
+                }
+            }
+            wykres.Series.Remove(seriaToDelete);
         }
     }
 }
